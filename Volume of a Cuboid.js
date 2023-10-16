24a48e9a1c76e07a4e5e558a3ab88851b863a038
Oleg Shaliukov <heilagr.ercera@gmail.com>
2023-10-16 17:36:06 +0200

    add new tasks

---
+++ Volume of a Cuboid.js
@@ -0,0 +1,26 @@
+// Solution / Решение
+
+class Kata {
+    static getVolumeOfCuboid(l, w, h) {
+        return l * w * h
+    }
+}
+
+// Tests
+
+console.log(Kata.getVolumeOfCuboid(1, 2, 2)); // 4
+console.log(Kata.getVolumeOfCuboid(6, 2, 5)); // 60
+
+/*
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, 
+width and height of the cuboid. Write a function to help Bob with this calculation.
+
+*/
+
+/*
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, 
+width and height of the cuboid. Write a function to help Bob with this calculation.
+
+*/
