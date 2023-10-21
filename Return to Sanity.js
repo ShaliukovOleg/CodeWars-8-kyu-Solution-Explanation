1c655f8f36dac6248ed6b64b04d217f9f6e85d21
Oleg Shaliukov <heilagr.ercera@gmail.com>
2023-10-21 21:17:25 +0200

    add new tasks

---
+++ Return to Sanity.js
@@ -0,0 +1,23 @@
+// Solution / Решение
+
+function mystery() {
+    var results =
+        { sanity: 'Hello' };
+    return results;
+}
+
+/*
+
+This function should return an object, 
+but it's not doing what's intended. 
+What's wrong?
+
+*/
+
+/*
+
+Эта функция должна возвращать объект, 
+но она не делает того, что задумано. 
+Что не так?
+
+*/
